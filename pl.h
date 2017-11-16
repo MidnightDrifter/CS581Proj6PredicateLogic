ea8d82f22410a0a3ba684d642770c02b4debef4b
MidnightDrifter <sean.higgins@digipen.edu>
2017-11-15 23:53:48 -0800

    CNF, clause stuff set up hopefully

--- pl.h
+++ pl.h
@@ -35,89 +35,283 @@ class Literal {
         }
         ////////////////////////////////////////////////////////////////////////
         Literal operator~() const { 
-            Literal result( *this );
-            result.Negate();
-            return result;
-        }
-        ////////////////////////////////////////////////////////////////////////
-        bool Complementary( Literal const& op2 ) const {
-            return ( name == op2.name ) && ( negated != op2.negated );
-        }
-        ////////////////////////////////////////////////////////////////////////
-        friend std::ostream& operator<<( std::ostream& os, Literal const& literal ) {
-            os << (literal.negated?"~":"") << literal.name;
-            return os;
-        }
-    private:
-        std::string name;
-        bool negated;
+Literal result(*this);
+result.Negate();
+return result;
+		}
+		////////////////////////////////////////////////////////////////////////
+		bool Complementary(Literal const& op2) const {
+			return (name == op2.name) && (negated != op2.negated);
+		}
+		////////////////////////////////////////////////////////////////////////
+		friend std::ostream& operator<<(std::ostream& os, Literal const& literal) {
+			os << (literal.negated ? "~" : "") << literal.name;
+			return os;
+		}
+	private:
+		std::string name;
+		bool negated;
 };
 
-class Clause {
-    public:
-        // ..........
-        // ..........
-        // ..........
-        // ..........
-        ////////////////////////////////////////////////////////////////////////
-        friend std::ostream& operator<<( std::ostream& os, Clause const& clause ) {
-            unsigned size = clause.literals.size();
+class Clause {   //Bunch of Literals OR'd together
+public:
+	// ..........
+	// ..........
+	// ..........
+	// ..........
+	////////////////////////////////////////////////////////////////////////
+	Clause( Literal const& l) : literals() { literals.insert(l); }
+	//Change this to bool maybe?  idk
+	void Insert( Literal const& l) { literals.insert(l); }
+	bool operator<( Clause const& other) const
+	{
+		//for(auto myIt = literals.begin())
 
-            if ( clause.size() == 0 ) {
-                os << " FALSE ";
-            } else {
-                std::set< Literal >::const_iterator it = clause.literals.begin();
-                os << "( " << *it;
-                ++it;
-                for ( ; it!=clause.literals.end(); ++it ) {
-                    os << " | " << *it;
-                }
-                os << " ) ";
-            }
-            return os;
-        }
-    private:
-        std::set< Literal > literals;
+		if (*(literals.end()) < (*(other.literals.begin() )))
+		{
+			return true;
+		}
+
+		else if(*(other.literals.end()) < (*(literals.begin())))
+		{
+			return false;
+		}
+
+		else
+		{
+			int iterOffset = 0;
+			for (auto myIter = literals.begin(); myIter != literals.end();)  //myIter++
+			{
+
+				auto otherIter = other.literals.begin();
+				for (int i = 0; i < iterOffset; i++)
+				{
+					if (otherIter != other.literals.end())
+					{
+						otherIter++;// iterOffset;
+					}
+					else {
+						otherIter = other.literals.end();
+						i = iterOffset;
+					}
+				}
+
+
+				//Thing at position iterOffset in THIS clause is strictly less than thing at position iterOffset in OTHER clause
+				if (*myIter < *otherIter)
+				{
+					return true;
+				}
+
+				//Thing at position iterOffset in OTHER clause is strictly less than thing at position iterOffset in THIS clause
+				else if (*myIter < *otherIter)
+				{
+					return false;
+				}
+
+
+				if (myIter != literals.end())
+				{
+					iterOffset++;
+					myIter++;
+				}
+
+
+
+			}
+
+
+			return false;
+		}
+
+	}
+
+		
+
+	/*
+	Clause const operator>(Clause const& other) const
+	{
+
+	}
+
+	Clause const operator&(Clause const& other) const
+	{
+
+	}
+
+	Clause const operator|(Clause const& other) const
+	{
+
+	}
+
+	Clause const operator~() const
+	{
+
+	}
+	*/
+
+	friend std::ostream& operator<<(std::ostream& os, Clause const& clause) {
+		unsigned size = clause.literals.size();
+
+		if (size == 0) {
+			os << " FALSE ";
+		}
+		else {
+			std::set< Literal >::const_iterator it = clause.literals.begin();
+			os << "( " << *it;
+			++it;
+			for (; it != clause.literals.end(); ++it) {
+				os << " | " << *it;
+			}
+			os << " ) ";
+		}
+		return os;
+	}
+//private:
+	std::set< Literal > literals;
 };
 
-class CNF {
-    public:
-        // ..........
-        // ..........
-        // ..........
-        // ..........
-        ////////////////////////////////////////////////////////////////////////
-        // not
-        CNF const operator~( ) const {
-            //if CNF is made of a single clause: A | B | ~C,
-            //negating it gives ~A & ~B & C (3 clauses)
-            //otherwise
-            //CNF = clause1 & clause2 & clause3,
-            //~CNF = ~clause1 | ~clause2 | ~clause3 
-            //"or" is defined later 
-        }
-        ////////////////////////////////////////////////////////////////////////
-        // =>
-        CNF const operator>( CNF const& op2 ) const {
-            CNF const& op1 = *this;
-            return ~(op1)|op2;
-        }
-        ////////////////////////////////////////////////////////////////////////
-        // and
-        CNF const operator&( CNF const& op2 ) const {
-            //CNF1 = clause1 & clause2 & clause3,
-            //CNF2 = clause4 & clause5 & clause6,
-            //CNF1 & CNF2 = clause1 & clause2 & clause3 & clause4 & clause5 & clause6
-        }
-        ///////////////////////////////////////////////////////////////////////
-        // or
-        CNF const operator|( CNF const& op2 ) const {
-            //CNF1 = clause1 & clause2 & clause3,
-            //CNF2 = clause4 & clause5 & clause6,
-            //CNF1 | CNF2 = 
-            //              c1|c4 & c1|c5 & c1|c6    &
-            //              c2|c4 & c2|c5 & c2|c6    &
-            //              c3|c4 & c3|c5 & c3|c6
+class CNF {   //Bunch of Clauses AND'd together
+public:
+	// ..........
+	// ..........
+	// ..........
+	// ..........
+	////////////////////////////////////////////////////////////////////////
+	
+	CNF( Clause const& c) : clauses() { clauses.insert(c); }
+	CNF( Literal const& l) : clauses() { clauses.insert(Clause(l)); }
+	CNF() : clauses() {}
+	
+	
+	// not
+	CNF const operator~() const {   //Aren't all of these supposed to act on the current, given CNF?
+		//if CNF is made of a single clause: A | B | ~C,
+		//negating it gives ~A & ~B & C (3 clauses)
+		//otherwise
+		//CNF = clause1 & clause2 & clause3,
+		//~CNF = ~clause1 | ~clause2 | ~clause3 
+		//"or" is defined later 
+
+		CNF & op1 = CNF(*this);  //Add or remove const from this?   
+
+		if (0==clauses.size())  //SOMETHING HAS GONE HORRIBLY HORRIBLY WRONG
+		{
+			return op1;
+		}
+
+
+		else if (clauses.size() == 1)  //
+		{
+			auto myLiterals = op1.clauses.begin();
+			for (auto literalsIter = myLiterals->literals.begin(); literalsIter != myLiterals->literals.end(); literalsIter++)
+			{
+			//	Literal l(*literalsIter);
+			//	Clause c(~(l));
+			//	Clause c(~(*literalsIter));
+			//	clauses.insert(c);
+
+
+			//	clauses.insert(const_cast<Clause&>(Clause(~(*literalsIter))));
+				op1.clauses.emplace(~(*literalsIter));
+			
+				//clauses.insert(const_cast<Literal&>(*literalsIter));
+			
+				//Literal l(*literalsIter);
+				//clauses.emplace(l);
+			
+			
+			}
+			return op1;
+		}
+
+		
+
+		else
+		{
+			//CNF out;
+			auto opClauses = clauses.begin();
+			CNF out(~ CNF(*opClauses));
+			opClauses++;
+			for ( ;opClauses != op1.clauses.end(); opClauses++)
+			{
+				out = out | (~ CNF(*opClauses));
+			}
+
+			return out;
+		}
+
+
+
+
+
+
+	}
+	////////////////////////////////////////////////////////////////////////
+	// =>
+	CNF const operator>(CNF const& op2) const {
+		CNF const& op1 = *this;
+		return ~(op1) | op2;
+	}
+	////////////////////////////////////////////////////////////////////////
+	// and
+	CNF const operator&(CNF const& op2) const {   //Gotta check this, CONFUSED
+		//CNF1 = clause1 & clause2 & clause3,
+		//CNF2 = clause4 & clause5 & clause6,
+		//CNF1 & CNF2 = clause1 & clause2 & clause3 & clause4 & clause5 & clause6
+
+
+		CNF out(*this);
+		for (auto otherClausesIter = op2.clauses.begin(); otherClausesIter != op2.clauses.end(); otherClausesIter++)
+		{
+			out.clauses.insert(*otherClausesIter);
+		}
+
+		return out;
+
+
+	}
+	///////////////////////////////////////////////////////////////////////
+	// or
+	CNF const operator|(CNF const& op2) const {
+		//CNF1 = clause1 & clause2 & clause3,
+		//CNF2 = clause4 & clause5 & clause6,
+		//CNF1 | CNF2 = 
+		//              c1|c4 & c1|c5 & c1|c6    &
+		//              c2|c4 & c2|c5 & c2|c6    &
+		//              c3|c4 & c3|c5 & c3|c6
+
+		//output CNF out = this
+		//for each literal in THIS = i
+			//for each literal in op2 = j
+				// out +=  i|j
+
+
+		CNF out;
+
+		for (auto thisIter = this->clauses.begin(); thisIter != clauses.end(); thisIter++)
+		{
+			for (auto otherIter = op2.clauses.begin(); otherIter != op2.clauses.end(); otherIter++)
+			{
+				//out.clauses.emplace((*thisIter) | (*otherIter));
+
+				
+				for (auto thisClauseIter = (*thisIter).literals.begin(); thisClauseIter != (*thisIter).literals.end(); thisClauseIter++)
+					{
+						for (auto otherClauseIter = (*otherIter).literals.begin(); otherClauseIter != (*otherIter).literals.end(); otherClauseIter++)
+							{
+								out.clauses.emplace((*thisClauseIter) | (*otherClauseIter));
+							}
+
+					}
+
+					
+
+				}
+			}
+			
+		return out;
+
         }
 
         /////////////////////////////////////////////////////////////////////////////////
@@ -126,7 +320,7 @@ class CNF {
         CNF const operator|( Literal const& op2 ) const { return operator|( CNF(op2) ); }
 
         ////////////////////////////////////////////////////////////////////////
-        bool Empty() const { return literals.size()==0; }
+        bool Empty() const { return clauses.size()==0; }
         ////////////////////////////////////////////////////////////////////////
         std::set< Clause >::const_iterator begin() const { return clauses.begin(); }
         std::set< Clause >::const_iterator end()   const { return clauses.end(); }
